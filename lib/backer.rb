b04562c7194653807cf0050e4936c06d2d93e92e
Nahum <losond8@gmail.com>
2021-07-01 17:15:06 -0500

    commit tree 47635e8196c9cfed96a51442df1fa1abac48f567

---
+++ lib/backer.rb
@@ -0,0 +1,27 @@
+class Backer
+    @@all = []
+
+    def self.all
+        @@all
+    end
+
+    attr_reader :name
+
+    def initialize name
+        @name = name
+        @@all << self
+    end
+
+    def back_project project
+        ProjectBacker.new(project, self )
+    end
+
+    def backed_projects
+        project_backers = ProjectBacker.all.select do |pb|
+          pb.backer == self
+        end
+        project_backers.map do |pb|
+          pb.project
+        end
+      end
+end
